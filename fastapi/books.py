287a7a0c11748c5a33043922b911788e918cdf2b
sebastianiv21 <seluti98@gmail.com>
2023-09-12 22:42:04 -0500

    add half of first project

---
+++ fastapi/books.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+BOOKS = [
+    {"title": "Title One", "author": "Author One", "category": "science"},
+    {"title": "Title Two", "author": "Author Two", "category": "science"},
+    {"title": "Title Three", "author": "Author Three", "category": "history"},
+    {"title": "Title Four", "author": "Author Four", "category": "math"},
+    {"title": "Title Five", "author": "Author Five", "category": "math"},
+    {"title": "Title Six", "author": "Author Two", "category": "math"},
+]
+
+app = FastAPI()
+
+# create endpoints
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+# dynamic parameters
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get("title").casefold() == book_title.casefold():
+            return book
---
+++ func_assignment.py
@@ -0,0 +1,3 @@
+def create_dict(firstname, lastname, age):
+    dict = {"first_name": firstname, "last_name": lastname, "age": age}
+    return dict
